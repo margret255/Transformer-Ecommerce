a7a24466cb485bf6f0a0240a368bebf4102e01dc
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2024-12-15 12:22:42 +0300

    Update Routes.kt

--- app/src/main/java/com/example/transformerecommerce/navigation/Routes.kt
+++ app/src/main/java/com/example/transformerecommerce/navigation/Routes.kt
@@ -3,3 +3,5 @@ package com.example.transformerecommerce.navigation
 const val HOME_URL = "Home"
 const val REGISTER_URL = "register"
 const val LOGIN_URL = " login"
+const val ADD_PRODUCTS_URL="add_products"
+const val VIEW_PRODUCTS_URL="view_products_url"
